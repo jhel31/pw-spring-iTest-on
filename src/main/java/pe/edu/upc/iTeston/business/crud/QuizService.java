46d72daf261ebb1f7cbf1624e4a3729e9d5a9acc
jhel31 <adrianoesq11@gmail.com>
2021-11-16 12:20:11 -0500

    Revert "Revert "Revert "UPDATES"""
    This reverts commit 6cb11361703445e494182e2e941f6b73cbe2d44a.

--- src/main/java/pe/edu/upc/iTeston/business/crud/QuizService.java
+++
@@ -1,9 +0,0 @@
-package pe.edu.upc.iTeston.business.crud;
-
-import java.util.List;
-
-import pe.edu.upc.iTeston.models.entities.Quiz;
-
-public interface QuizService extends CrudService<Quiz, String>{
-	List<Quiz> findByUniversityId(String id) throws Exception;
-}
